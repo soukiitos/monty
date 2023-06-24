bd3173c8cf32803b5dadc8c00c2352dc5d14f7a5
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-06-23 17:50:23 -0700

    Stacks, Queues, LIFO, FIFO

--- pchar_s.c
+++ pchar_s.c
@@ -11,17 +11,17 @@ void pchar_s(stack_t **h, unsigned int size)
 	stack_t *head;
 
 	head = *h;
-	if (head->n > 127 || head->n < 0)
+	if (!head)
 	{
-		fprintf(stderr, "L%d: can't pchar, value out of range\n", size);
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", size);
 		fclose(val.file);
 		free(val.ch);
 		stack_free(*h);
 		exit(EXIT_FAILURE);
 	}
-	if (!head)
+	if (head->n > 127 || head->n < 0)
 	{
-		fprintf(stderr, "L%d: can't pchar, stack empty\n", size);
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", size);
 		fclose(val.file);
 		free(val.ch);
 		stack_free(*h);
